a25417e5f2191a81188db38a433214e47a5a3cfc
Mahesh Chand <d.mahesh0307@gmail.com>
2022-03-28 01:54:30 +0530

    aws-spring-example-example code base

---
+++ src/main/java/com/tech/aws/AwsSpringElasticBeanExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tech.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringElasticBeanExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringElasticBeanExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/aws/AwsSpringElasticBeanExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringElasticBeanExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
